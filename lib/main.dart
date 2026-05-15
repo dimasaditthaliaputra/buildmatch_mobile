5f1712b3ea1bcc150ca162fe15c9596eb107ec2b
dimasaditt <aditcraftyt@gmail.com>
2026-05-15 17:55:29 +0700

    feat : add env for security

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'services/supabase_service.dart';
 import 'services/push_notification_service.dart';
 import 'config/injection_container.dart' as di;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
 
   await SupabaseService.initialize();
 
